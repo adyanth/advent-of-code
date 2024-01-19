c970348d2ec1de32ec462ee7e7db2b7ea23b4b60
Adyanth Hosavalike <ahosavalike@belvederetrading.com>
2024-01-19 13:04:09 -0700

    :sparkles: Day 2 part 2

--- aoc2015/src/day2.rs
+++ aoc2015/src/day2.rs
@@ -13,8 +13,19 @@ pub fn part1(dims: &[Dim]) -> i32 {
     dims.iter()
         .map(|dim| {
             assert!(dim.len() == 3);
-            2 * (dim[0] * dim[1] + dim[1] * dim[2] + dim[2] * dim[0])
-                + dim.iter().product::<i32>() / dim.iter().max().unwrap()
+            2 * (dim[0] * dim[1] + dim[1] * dim[2] + dim[2] * dim[0])   // Necessary to wrap
+                + dim.iter().product::<i32>() / dim.iter().max().unwrap() // Extra
+        })
+        .sum()
+}
+
+#[aoc(day2, part2)]
+pub fn part2(dims: &[Dim]) -> i32 {
+    dims.iter()
+        .map(|dim| {
+            assert!(dim.len() == 3);
+            2 * (dim.iter().sum::<i32>() - dim.iter().max().unwrap())    // Wrap around
+                + dim.iter().product::<i32>() //  Bow
         })
         .sum()
 }
