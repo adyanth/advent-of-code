1456a6f75567c325a96f525407efa0d669ed89e6
Adyanth Hosavalike <ahosavalike@belvederetrading.com>
2024-01-19 12:31:42 -0700

    :sparkles: Solve day 1 part 2

--- aoc2015/src/day1.rs
+++ aoc2015/src/day1.rs
@@ -6,3 +6,21 @@ pub fn part1(input: &[u8]) -> i32 {
         _ => 0,
     }).sum()
 }
+
+#[aoc(day1, part2)]
+pub fn part2(input: &[u8]) -> i32 {
+    input.into_iter().map(|&i| match i {
+        b'('=> 1,
+        b')'=> -1,
+        _ => 0,
+    }).enumerate().reduce(|(acci, acce), (i, e)| {
+        if acci != 0 {
+            (acci, acce)
+        }
+        else if acce + e < 0 {
+            (i + 1, acce)
+        } else {
+            (0, acce + e)
+        }
+    }).unwrap().0 as i32
+}
