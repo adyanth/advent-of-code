7ce7254adba147ec10ed8b359fe04a477158ee13
Adyanth Hosavalike <ahosavalike@belvederetrading.com>
2024-01-19 12:07:36 -0700

    :tada: Set up AOC

---
+++ aoc2015/src/day1.rs
@@ -0,0 +1,4 @@
+#[aoc(day1, part1)]
+pub fn part1(_: &[u8]) -> i32 {
+    0
+}
---
+++ aoc2015/src/main.rs
@@ -0,0 +1,6 @@
+extern crate aoc_runner;
+extern crate aoc_runner_derive;
+
+extern crate aoc2015;
+use aoc_runner_derive::aoc_main;
+aoc_main! { lib = aoc2015 }
