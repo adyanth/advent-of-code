bd982b4374f6c8e2d341d9e79ef4916ebdbc8a9e
Adyanth Hosavalike <ahosavalike@belvederetrading.com>
2024-01-19 14:16:57 -0700

    :sparkles: D4P1

---
+++ aoc2015/src/day4.rs
@@ -0,0 +1,12 @@
+use md5;
+
+#[aoc(day4, part1)]
+pub fn part1(input: &str) -> i32 {
+    let mut i = 1;
+    let mut hash = md5::compute(format!("{input}{i}")).0;
+    while !hash.starts_with(&[0, 0]) || hash[2] >= 16 {
+        i += 1;
+        hash = md5::compute(format!("{input}{i}")).0;
+    }
+    i
+}
