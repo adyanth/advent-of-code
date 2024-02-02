466f06ece8e40a30d04953b203bc24e1903d8d42
Adyanth Hosavalike <ahosavalike@belvederetrading.com>
2024-02-02 12:56:45 -0700

    Optimize string concat

--- aoc2015/src/day4.rs
+++ aoc2015/src/day4.rs
@@ -1,12 +1,29 @@
 use md5;
 
-#[aoc(day4, part1)]
-pub fn part1(input: &str) -> i32 {
+#[aoc(day4, part1, base)]
+pub fn part1_base(input: &str) -> i32 {
     let mut i = 1;
     let mut hash = md5::compute(format!("{input}{i}")).0;
     while !hash.starts_with(&[0, 0]) || hash[2] >= 16 {
         i += 1;
         hash = md5::compute(format!("{input}{i}")).0;
+    }
+    i
+}
+
+#[aoc(day4, part1, optimized)]
+pub fn part1_optimized(input: &str) -> i32 {
+    let mut i = 1;
+    let il = input.len();
+    let mut c = String::with_capacity(il + 7);
+    c.clone_from(&input.to_string());
+    c.insert_str(il, &i.to_string());
+    let mut hash = md5::compute(&c).0;
+    while !hash.starts_with(&[0, 0]) || hash[2] >= 16 {
+        i += 1;
+        c.truncate(il);
+        c.insert_str(il, &i.to_string());
+        hash = md5::compute(&c).0;
     }
     i
 }
